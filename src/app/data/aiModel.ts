0b43d6ad54185ac1e7e847f0e83fa5e2398bab2b
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-05-07 19:12:44 +0530

    Completed

--- src/app/data/aiModel.ts
+++ src/app/data/aiModel.ts
@@ -1,4 +1,9 @@
-const modelVersion: number = 0;
-const models: string[] = ["gemini-2.5-flash", "gemini-3-flash-preview"];
+const modelVersion: number = 2;
+const models: string[] = [
+  "gemini-2.5-flash",
+  "gemini-3-flash-preview",
+  "gemini-3.1-flash-lite-preview",
+  "gemini-3.1-flash-live-preview",
+];
 
 export { modelVersion, models };
